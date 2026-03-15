b8a186bc7b5aa055ccfc44af1cea0580041a8c32
Balakumaran <kumaranbala216@gmail.com>
2026-03-15 19:28:15 +0530

    Create 14_if-else(greatest).py

---
+++ 01_conditional statement/14_if-else(greatest).py
@@ -0,0 +1,15 @@
+#QUESTION-->WAP TO FIND GREATEST OF TWO NUMBERS
+
+#ALGORITHM-->
+#step1:Get the input two numbers as integer from the user.
+#step2:if num1 is greater than num2 print num1 is greater. Else print num2 is greater
+
+
+
+#PROGRAM-->
+num1=int(input("Enter a number 1:")
+num2=int(input("Enter a number 2:")
+if num1>num2:
+         print("num1 is greater")
+else:
+         print("num2 is greater")
