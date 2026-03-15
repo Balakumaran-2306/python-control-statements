2bbcccb86ad7aea772666b157d44d76be3b92c73
Balakumaran <kumaranbala216@gmail.com>
2026-03-15 19:38:26 +0530

    Create 15_if-else(vote eligible).py

---
+++ 01_conditional statement/15_if-else(vote eligible).py
@@ -0,0 +1,14 @@
+#QUESTIION-->WAP TO READ THE AGE OF A CANDIDATE AND DETERMINE WHETHER THE CANDIDATE IS ELIGIBLE TO CAST HIS OWN VOTE
+
+#ALGORITHM-->
+#step1:Get the age as integer from the user.
+#step2:check whether age is greater than 18 print eligible else print not eligible.
+
+
+
+#PROGRAM-->
+age=int(input("Enter your age:"))
+if age>=18:
+  print("ELIGIBLE")
+else:
+  print("NOT ELIGIBLE")
